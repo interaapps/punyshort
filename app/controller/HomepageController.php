187d237c1717f97e8dd8b8eb349a32aef7a2e719
JulianFun123 <julianfun123@yahoo.com>
2020-05-26 10:16:52 +0200

    Added / for custom domains

--- app/controller/HomepageController.php
+++ app/controller/HomepageController.php
@@ -6,6 +6,9 @@ use ulole\core\classes\Response;
 class HomepageController {
 
     public static function page() {
+        if((new \databases\ShortlinksTable)->count()->where("name", "/")->andwhere("domain", $_SERVER['SERVER_NAME'])->get() > 0) {
+            $_ROUTEVAR[1] = "/";
+        }
         return view("homepage", [
             "domains"=>(new \databases\DomainsTable)->select("domain_name")->where("is_default", "1")->andwhere("is_public", "1")->get()
         ]);
