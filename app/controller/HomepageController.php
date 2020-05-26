e3387b8ec190be580aa45b575759f8beb54092b2
JulianFun123 <julianfun123@yahoo.com>
2020-05-26 10:22:44 +0200

    Added / for custom domains

--- app/controller/HomepageController.php
+++ app/controller/HomepageController.php
@@ -8,6 +8,8 @@ class HomepageController {
     public static function page() {
         if((new \databases\ShortlinksTable)->count()->where("name", "/")->andwhere("domain", $_SERVER['SERVER_NAME'])->get() > 0) {
             $_ROUTEVAR[1] = "/";
+            \app\controller\LinkController::redirect();
+            return;
         }
         return view("homepage", [
             "domains"=>(new \databases\DomainsTable)->select("domain_name")->where("is_default", "1")->andwhere("is_public", "1")->get()
